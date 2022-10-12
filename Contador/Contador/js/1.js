7b74f498796703d5dd6a9ae8faf5c7a870f4097e
Jonathan Balarezo <jse.balarezo@yavirac.edu.ec>
2022-10-11 20:08:11 -0500

    sonidos

--- Contador/Contador/js/1.js
+++ Contador/Contador/js/1.js
@@ -1,9 +1,6 @@
-const sonidos = document.getElementById('sonidos');
+let sonidos = new Audio('../sonido/uno.mp3');
 
-document.addEventListener('keydown', function(evento){
-    if(evento.keyCode == 32) {
-        sonidos.innerHTML += '<audio src="../sonido/uno.mp3" autoplay></audio>';
-    }
-})
-
+setInterval(()=>{
+   sonidos.play()
+}, 3000)
 
